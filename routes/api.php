fa30c3303060e162952bf4cd675f92ce7a6b317b
Danil Afanasev <afanasevdanilfed@gmail.com>
2024-11-01 17:29:50 +0500

    Update project configs

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TokenController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/sanctum/token', TokenController::class);
+
+Route::controller(RoleController::class)->group(function () {
+    Route::post('/roles', 'store');
+    Route::get('/roles', 'index');
+    Route::get('/roles/{role}', 'index');
+    Route::patch('/roles/{role}', 'update');
+    Route::delete('/roles/{role}', 'destroy');
+});
+
+// Route::middleware(['auth:sanctum'])->group(function () {
+// });
