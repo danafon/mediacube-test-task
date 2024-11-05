243b0886890af888c26e5931d12b2d2560b58dfb
Danil Afanasev <afanasevdanilfed@gmail.com>
2024-11-05 16:37:21 +0500

    Fix the startup logic

--- launch.sh
+++ launch.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+echo "Installing Sail..."
+./vendor/bin/sail composer install
+
+echo "Fill the .env file..."
+cp .env.example .env
+
+echo "Generate a key..."
+./vendor/bin/sail artisan key:generate
+
 echo "Starting Sail..."
 ./vendor/bin/sail up -d
 
